5da0e01e5bf3a692879050878b1208c362a7e3ec
ravidhwn01 <87488989+ravidhwn01@users.noreply.github.com>
2022-06-12 00:14:51 +0530

    Update dsa1.cpp

--- dsa1.cpp
+++ dsa1.cpp
@@ -1,12 +1,28 @@
+
 #include <iostream>
 using namespace std;
- 
 int main()
 {
-     int a = 34;
-     int b;
-     b= a++;
-     cout<<a<<endl;  
-     cout<<b<<endl;  
-    return 0;
+	int x = 10, a;
+
+	a = ++x;
+	cout << "Pre Increment Operation";
+
+	// Value of a will change
+	cout << "\na = " << a;
+
+	// Value of x change before execution of a=++x;
+	cout << "\nx = " << x;
+ 
+  a = x++;
+ 
+    cout << "Post Increment Operation";
+ 
+    // Value of a will not change
+    cout << "\na = " << a;
+ 
+    // Value of x change after execution of a=x++;
+    cout << "\nx = " << x;
+
+	return 0;
 }
