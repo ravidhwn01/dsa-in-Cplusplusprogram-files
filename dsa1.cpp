4f0b9dff9a3f485f146ba404ccebf23929130a6f
ravidhwn01 <87488989+ravidhwn01@users.noreply.github.com>
2022-06-12 00:03:15 +0530

    Add files via upload

---
+++ dsa1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class  demo
+{ 
+    private: 
+     
+    public: 
+     
+}; 
+int main()
+{
+     int a = 34;
+     int b;
+     b= a++;
+     cout<<a<<endl;  
+     cout<<b<<endl;  
+    return 0;
+}
