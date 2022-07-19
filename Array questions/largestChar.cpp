2f8faa548e470c8c12b942e509e0ed4abfe9d8e8
ravidhwn01 <87488989+ravidhwn01@users.noreply.github.com>
2022-07-19 18:24:09 +0530

    largest word in a sentence

---
+++ Array questions/largestChar.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // largest word in a sentence
+    int n;
+    cin>>n;
+    cin.ignore();
+    char arr[n+1];
+    cin.getline(arr,n);
+    cin.ignore();
+   int i =0;
+   int currlen= 0, maxlen = 0;
+        int st = 0,maxst = 0;
+    while (1)
+    { 
+        if (arr[i] == ' ' || arr[i] == '\0'){
+            if(currlen>maxlen){
+                maxlen = currlen;
+                maxst = st;
+            }
+            currlen = 0;
+            st = i+1;
+        }
+       else
+       currlen++;
+        if(arr[i] =='\0'){
+            break;
+        }
+        i++;
+    }
+    cout<<maxlen<<endl;
+    cout<<maxst<<endl;
+
+    for (int i = 0; i < maxlen; i++)
+    {
+      cout<<arr[i+maxst];
+    }
+    
+    return 0;
+}
