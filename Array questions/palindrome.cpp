33c7f6159c1014c62196a109ef85def78a96aa5e
ravidhwn01 <87488989+ravidhwn01@users.noreply.github.com>
2022-07-20 18:46:07 +0530

    palindrome string

---
+++ Array questions/palindrome.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // find a  palindrome sentence
+    int n;
+    cin >> n;
+
+    char arr[n + 1];
+
+    cin >> arr;
+    bool check = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != arr[n - 1 - i])
+        {
+            check = 0;
+            break;
+        }
+    }
+    if (check == true)
+    {
+        cout << "word is a palindrome" << endl;
+    }
+    else
+    {
+        cout << "word is not a palindrome" << endl;
+    }
+
+    return 0;
+}
