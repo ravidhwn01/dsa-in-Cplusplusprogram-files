43caa1efb7d23b21c82858131e47b65351a2fb2b
ravidhwn01 <87488989+ravidhwn01@users.noreply.github.com>
2022-07-21 23:24:43 +0530

    revese of a string

---
+++ Array questions/rverseOfString.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void reverse(string s){
+    if(s.length()==0)
+    {return;}
+ 
+string ros = s.substr(1);
+reverse(ros);
+cout<<s[0];
+}
+int main()
+{
+    reverse("ravidhawan");
+    
+    return 0;
+}
