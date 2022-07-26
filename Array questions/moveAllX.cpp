eb0f1a8ab65839fee02d185b17b721bf81457bb8
ravidhwn01 <87488989+ravidhwn01@users.noreply.github.com>
2022-07-26 22:24:44 +0530

    Add files via upload

---
+++ Array questions/moveAllX.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+string moveAllX(string s){
+    if(s.length()==0){
+return "";
+    }
+    char ch = s[0];
+    string ans = moveAllX(s.substr(1));
+    if(ch =='x'){
+        return ans +ch;
+
+}
+return ch + ans;
+}
+int main()
+{
+    cout<<moveAllX("xxxxxdfdssdddxxxxxhxxhzzzx");
+    
+    return 0;
+}
